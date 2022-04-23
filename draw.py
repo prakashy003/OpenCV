830c1f5f53bed7ff6102f77dc018dc0e762b02e0
prakashy003 <prakashy2901@gmail.com>
2022-04-23 06:32:31 +0530

    Learnt how to draw shapes and write text

---
+++ draw.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+
+"""img=cv.imread('Resources/Photos/cat.jpg')
+cv.imshow('cat', img)"""
+
+#Draw in a blank image
+blank=np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Blank', blank)
+
+#point the image a certain color
+# blank[:]=0,255,0
+# cv.imshow('Green', blank)
+
+#draw a rectangle
+# cv.rectangle(blank,(0,0), (250,250), (255,255,0), thickness=2)
+# cv.imshow('Rectangle', blank)
+
+# cv.rectangle(blank,(0,0), (250,250), (255,255,0), thickness=cv.FILLED)
+# cv.imshow('Rectangle', blank)
+
+cv.rectangle(blank,(0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
+
+
+#draw a circle
+cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255),thickness=3)
+cv.imshow('Circle', blank)
+
+#draw a line
+cv.line(blank,(0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
+
+#write a text
+cv.putText(blank, 'Hello, My name is Prakash!!', (0,400), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255,255,0), thickness=2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
